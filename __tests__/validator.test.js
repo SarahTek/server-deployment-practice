838e7071acc0d3978eff91a204b03f8f1f09c8e7
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-07-15 01:53:17 -0400

    WOrking on lab 02 express

---
+++ __tests__/validator.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const {validator} = require('../src/middleware/validator');
+describe('validator', () => {
+  it('calls next()', () => {
+    const req = { method: 'GET', url: '/person', params: {name: 'sarah'} };
+    const res = {};
+    const next = jest.fn();
+    validator(req, res, next);
+
+    // Assetion
+    expect(next).toHaveBeenCalled();
+  });
+})
---
+++ src/error-handlers/404.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Create a error-handlers folder and add 2 modules to it:
+// 404.js
+// Sends a 404/Not-Found message as the response
+// Import this into your server and set it up to be “used” after your other routes
+const makeError = (req, res) => {
+  res.status(404).send('Not-Found');
+}
+module.exports = {
+  makeError,
+}
